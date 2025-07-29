54155a5e5cd6119b0808d29f503f7cc3ace734a3
seraph <asoudani@student.1337.ma>
2025-07-29 17:09:19 +0100

    finished CPP05/ex00 but it needs recheck for possible mistakes..

---
+++ CPP05/ex01/Form.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoudani <asoudani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 17:00:25 by asoudani          #+#    #+#             */
+/*   Updated: 2025/07/29 17:07:50 by asoudani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include "Bureaucrat.hpp"
+
+class Form
+{
+	const STR Name;
+	bool	Signed;
+	const		
+	
+}
+
+#endif
